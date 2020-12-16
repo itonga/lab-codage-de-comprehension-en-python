1452c13ada2186fa6232cfb9d38762d6543e76b6
itonga <joitonga@gmail.com>
2020-12-16 12:11:06 -0500

    exo_03(2,4,6,8)

--- 03/programme.py
+++ 03/programme.py
@@ -4,9 +4,9 @@
 @author: CollegeBoreal
 """
 
-def exo_03(liste):
+def exo_03(2,4,6,8):
   # Selectionner une liste de nombre pairs uniquement
-  pairs = ??? 
+  pairs =[x*x for x in (2,4,6,8)]
   return sorted(pairs)
 
 def main():
